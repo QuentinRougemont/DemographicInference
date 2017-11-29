287977c55f8b0d428466880eec7cb96e1726a3ec
ubuntu <quentinrougemont@orange.fr>
2017-11-29 12:02:00 -0500

    bug fixed

--- 01.scripts/00-run_model.sh
+++ 01.scripts/00-run_model.sh
@@ -2,6 +2,7 @@
 
 fs=$1     #dataset fs stored in 03-data
 model=$2  #model names
+folded=$3 #either "folded" or "unfolded"
 
 if [[ -z "$fs" ]]
 then
@@ -15,9 +16,14 @@ then
     exit
 fi
 
-###Folded SFS
 for i in $(seq 1 5); 
 do
-    python 02-modifs/script_inference_anneal2_newton_mis_new_models_folded2.py -o 04-results/"$model"_$i -y pop1 -x pop2 -p 40,50,60
-    -f "$fs" -m "$model" -l -v  -z &>> 10-log/"$model"_"$i".log; 
+    if [ $folded = "folded" ]
+    then
+        python 02-modifs/script_inference_anneal2_newton_mis_new_models_folded2.py -o 04-results/"$model"_$i -y pop1 -x pop2 -p 40,50,60
+        -f "$fs" -m "$model" -l -v  -z &>> 10-log/"$model"_"$i".log;
+    else
+        python 02-modifs/unfolded/script_inference_anneal2_newton_mis.py -o 04-results/"$model"_$i -y pop1 -x pop2 -p 40,50,60
+        -f "$fs" -m "$model" -l -v  -z &>> 10-log/"$model"_"$i".log;
+   fi 
 done                                                                                                                                                                                                               
--- 01.scripts/reshape_aic.sh
+++
@@ -1,1 +0,0 @@
-tail -n 1 *masked*/*txt |grep -B 1 "AIC" |perl -pe 's/<==\n/\t/g' |less
