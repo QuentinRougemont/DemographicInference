8eed46f2eebeb13b2c6bbe436b6ae1a101a9b326
ubuntu <quentinrougemont@orange.fr>
2017-12-05 11:05:28 -0500

    move script in correct folder

---
+++ 01-scripts/graham_cedar/prepare_env_computecanada.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#example of creation of an environment containings old scipy packages
+
+#cd $HOME
+#cd DemographicInference
+module load gcc/4.8.5
+module load python/2.7.13
+mkdir temp
+virtualenv temp
+source temp/bin/activate
+pip install numpy
+pip install -Iv https://pypi.python.org/packages/2f/12/565c08132db50a0ba34a33e0901f3d1d4d72e3b432ea828e4d87be5a4991/scipy-0.13.3.tar.gz
+pip install matplotlib
+python setup.py install
+python -c 'from scipy import stats'
+cd 01-scripts
+python -c 'import dadi'
