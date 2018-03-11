519ffc329611cfeca6ceb33553dbe1ec7c8f24fb
ubuntu <quentinrougemont@orange.fr>
2018-03-11 11:27:37 -0400

    fixed script

--- 01-scripts/01-run_model_iteration.sh
+++ 01-scripts/01-run_model_iteration.sh
@@ -29,7 +29,7 @@ fi
 
 if [ $folded = "folded" ]
 then
-	python ../02-modifs/script_inference_anneal2_newton_mis_new_models_folded2.py -o "$model"_"$id" -y pop1 -x pop2 -p $grid_size,$grid_size2,$grid_size3  -f "$fs" -m "$model" -l -v  -z &>> ../10-log/"$model"_"$id".log;
+	python ../02-modifs/folded/script_inference_anneal2_newton_mis_new_models_folded2.py -o "$model"_"$id" -y pop1 -x pop2 -p $grid_size,$grid_size2,$grid_size3  -f "$fs" -m "$model" -l -v  -z &>> ../10-log/"$model"_"$id".log;
 else
     python 02-modifs/unfolded/script_inference_anneal2_newton_mis.py -o "$model"_$i -y pop1 -x pop2 -p 40,50,60
    -f "$fs" -m "$model" -l -v  -z &>> 10-log/"$model"_"$i".log;
