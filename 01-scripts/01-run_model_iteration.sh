e77e59f5d8a89f87ea80d909f6a153aa26d6bb04
ubuntu <quentinrougemont@orange.fr>
2018-04-04 02:47:23 -0400

    fixed bug

--- 01-scripts/01-run_model_iteration.sh
+++ 01-scripts/01-run_model_iteration.sh
@@ -40,7 +40,7 @@ then
         -z \
         &>> ../10-log/"$model"_"$id".log;
 else
-    python 02-modifs/unfolded/script_inference_anneal2_newton_mis.py \
+    python ../02-modifs/unfolded/script_inference_anneal2_newton_mis.py \
         -o "$model"_"$id" \
         -y pop1 \
         -x pop2 \
