d4bbe7d66405d070d281ee77002b3923cfcce34a
ubuntu <quentinrougemont@orange.fr>
2018-08-07 12:16:03 -0400

    modified script_inference_*.py output location

--- 01-scripts/00.run_dadi_parallel_v2.sh
+++ 01-scripts/00.run_dadi_parallel_v2.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #source temp/bin/activate
 # Move to directory where job was submitted
-cd 01-scripts
 
 #List of wanted arguments
 fs=$1     #dataset fs stored in 03-data
@@ -33,6 +32,6 @@ then
     exit
 fi
 
-nrep=64
+nrep=32
 NUM_CPUS=32
-seq $nrep |parallel -j "$NUM_CPUS" ./01-run_model_iteration_v2.sh {} "$fs" "$model" "$folded" "$grid_size"
+seq $nrep |parallel -j "$NUM_CPUS" ./01-scripts/01-run_model_iteration_v2.sh {} "$fs" "$model" "$folded" "$grid_size"
