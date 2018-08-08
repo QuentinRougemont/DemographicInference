31255848f929518aeee4dd569e18b763f29c5e21
ubuntu <quentinrougemont@orange.fr>
2018-08-08 10:33:04 -0400

    fixed minor bugs

--- 01-scripts/graham_cedar/00.run_dadi_cluster.sh
+++ 01-scripts/graham_cedar/00.run_dadi_cluster.sh
@@ -12,13 +12,13 @@
 ##PBS -l nodes=1:ppn=8
 ##SBATCH --mail-type=EA 
 
-source /home/quentin/software/DemographicInference/temp/bin/activate
+module load python/2.7.14
+source /home/path_to_activate_local_env/activate
 #source temp/bin/activate
 # Move to directory where job was submitted
 cd $SLURM_SUBMIT_DIR
 
-module load parallel/20170622
-
+#module load parallel/20170622 #useless
 #List of wanted arguments
 fs=$1     #dataset fs stored in 03-data
 model=$2  #model names
